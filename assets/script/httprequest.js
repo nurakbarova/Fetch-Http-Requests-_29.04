468bf817507060d5e4527db30d4204f5b4d520bf
nurakbarova <nurayakbarova76@gmail.com>
2023-04-30 12:30:32 +0400

    js

---
+++ assets/script/httprequest.js
@@ -0,0 +1,10 @@
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
+
+export const getAllCategories = async() => {
+    let globalData;
+   await fetch(baseURL)
+        .then(response => response.json())
+        .then(data => { globalData = data; }
+        )
+        return globalData
+}
--- script.js
+++
@@ -1,1 +0,0 @@
-const API_URL="https://jsonplaceholder.typicode.com/posts"
